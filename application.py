9f9d8d53c9996d82b26ad6057491e0ba36003273
deciob <decio.battaglia@gmail.com>
2024-07-27 18:35:37 +0100

    Added frozen-flask

--- application.py
+++ application.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
-@app.route("/<name>")
-def hello(name):
-    #return f"Hello, {escape(name)}!"
-    return render_template('hello.html', person=name)
+#@app.route("/<name>")
+#def hello(name):
+#    #return f"Hello, {escape(name)}!"
+#    return render_template('hello.html', person=name)
 
 with app.test_request_context():
     print(url_for('hello_world'))
-    print(url_for('hello', name="Decio"))
+    #print(url_for('hello', name="Decio"))
