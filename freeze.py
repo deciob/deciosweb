e7a1134a52d667456ef5274e639b1459c0fad8d9
deciob <decio.battaglia@gmail.com>
2024-08-09 17:05:06 +0100

    moved freeze build out of flaskr

--- freeze.py
+++ freeze.py
@@ -1,10 +1,13 @@
+import os
 from flask_frozen import Freezer
 import flaskr
 
 app = flaskr.create_app()
 
+app.config["FREEZER_DESTINATION"] = "../build"
+
 freezer = Freezer(app)
-import pdb; pdb.set_trace()
+
 
 if __name__ == '__main__':
     freezer.freeze()
