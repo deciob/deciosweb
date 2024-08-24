62629932453fc96caf188a1f34b16d7c01af9e65
deciob <decio.battaglia@gmail.com>
2024-08-24 17:35:03 +0100

    Lots of stuff added Basic layout and spacing and sizing Assets build in flask

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,12 +1,14 @@
-
-
 from flask import Flask
+from flask_assets import Environment, Bundle
 
 
 def create_app():
     # create and configure the app
     app = Flask(__name__)
+    assets = Environment(app)
 
+    css = Bundle('css-reset.css', 'variables.css', 'main.css', output='gen/packed.css')
+    assets.register('css_all', css)
 
     from . import home
     app.register_blueprint(home.bp)
@@ -16,5 +18,7 @@ def create_app():
 
     from . import visualisations
     app.register_blueprint(visualisations.bp)
+
+    #import pdb; pdb.set_trace()
 
     return app
--- freeze.py
+++ freeze.py
@@ -1,10 +1,10 @@
-import os
 from flask_frozen import Freezer
 import flaskr
 
 app = flaskr.create_app()
 
 app.config["FREEZER_DESTINATION"] = "../build"
+app.config["FREEZER_STATIC_IGNORE"] = "*.css"
 
 freezer = Freezer(app)
 
