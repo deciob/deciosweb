4523fb66831b9988e2ebfe7f18a7d7d8953e3947
deciob <decio.battaglia@gmail.com>
2024-09-07 19:57:05 +0100

    set freeze base url for github pages

--- freeze.py
+++ freeze.py
@@ -5,6 +5,7 @@ app = flaskr.create_app()
 
 app.config["FREEZER_DESTINATION"] = "../build"
 app.config["FREEZER_STATIC_IGNORE"] = "*.css"
+app.config['FREEZER_BASE_URL'] = "https://deciob.github.io/deciosweb/"
 
 freezer = Freezer(app)
 
