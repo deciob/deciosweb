f61139fc6006a39492d038b73e7dc0511a85623c
deciob <decio.battaglia@gmail.com>
2025-01-20 18:41:40 +0000

    removed old flask app

--- old-flask-app/flaskr/articles.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from flask import render_template
-
-bp = Blueprint("articles", __name__)
-
-@bp.route("/articles/")
-def articles():
-    return render_template('articles.html')
-
--- old-flask-app/flaskr/home.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from flask import render_template
-
-bp = Blueprint("homepage", __name__)
-
-@bp.route("/")
-def homepage():
-    return render_template('home.html', )
-
